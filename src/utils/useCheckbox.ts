ed4800aae56e0c646804b9e971509ba25c0d537c
yqianjiang <y.jiang.psy@gmail.com>
2021-10-11 13:06:43 +0800

    Update useCheckbox.ts

--- src/utils/useCheckbox.ts
+++ src/utils/useCheckbox.ts
@@ -1,11 +1,12 @@
 import { ref, onBeforeUpdate } from 'vue'
+import type { CheckboxInstance } from 'vant'
 
 export const useCheckbox = () => {
   const checked = ref([])
-  const checkboxRefs = ref([])
+  const checkboxRefs = ref<CheckboxInstance[]>([])
   const toggleCheckbox = (index: number) => {
     if (checkboxRefs.value[index]) {
-      checkboxRefs.value[index].toggle()
+      checkboxRefs.value[index]?.toggle()
     }
   }
   onBeforeUpdate(() => {
