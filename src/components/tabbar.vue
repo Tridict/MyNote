3410e3baa21d15e4fee4d8c8104617c5cd01ef93
yqianjiang <y.jiang.psy@gmail.com>
2022-03-27 18:19:21 +0800

    fix: tabbar

--- src/components/tabbar.vue
+++ src/components/tabbar.vue
@@ -1,41 +1,30 @@
 <template>
-  <van-tabbar v-model="active">
-    <van-tabbar-item name="notes" :badge="notesMsg">
-      <span>notes</span>
-      <template #icon="props">
-        <Icon name="notes" :active="props.active" />
-      </template>
-    </van-tabbar-item>
-    <van-tabbar-item name="tools">
-      <span>tools</span>
-      <template #icon="props">
-        <Icon name="tools" :active="props.active" />
-      </template>
-    </van-tabbar-item>
-    <van-tabbar-item name="me">
-      <span>me</span>
-      <template #icon="props">
-        <Icon name="me" :active="props.active" />
+  <van-tabbar route>
+    <van-tabbar-item
+      replace
+      :to="name"
+      :class="{ 'van-tabbar-item--active': props.current === name }"
+      v-for="(name, idx) in tabs"
+      :key="name"
+      :badge="msgs[idx]"
+    >
+      <span>{{ name }}</span>
+      <template #icon>
+        <Icon :name="name" :active="props.current === name" />
       </template>
     </van-tabbar-item>
   </van-tabbar>
 </template>
 
 <script setup lang="ts">
-import { ref, watch, defineProps } from 'vue'
-import { useRouter } from 'vue-router'
+import { ref, defineProps } from 'vue'
 import Icon from '@/components/icons/tabbar-icon.vue'
 
-const router = useRouter()
 const props = defineProps<{
   current: string
 }>()
-const active = ref(props.current)
-const notesMsg = ref('')
-
-watch(active, () => {
-  router.push('/' + active.value)
-})
+const msgs = ref(['', '', ''])
+const tabs = ref(['notes', 'tools', 'me'])
 </script>
 
 <style></style>
